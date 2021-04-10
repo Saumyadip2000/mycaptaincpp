45a2d36cf62adb673e0b22fb7233dd733d68e7fa
Saumyadip2000 <80485373+Saumyadip2000@users.noreply.github.com>
2021-04-10 23:10:19 +0530

    Create Task1_Q2.c
    Token and operators task 1 , question 2

---
+++ Task1_Q2.c
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float n1,n2,p;
+    cout<<"Enter two numbers:";
+    cin>>n1>>n2;
+    p=n1*n2;
+    cout<<"Product="<<p;
+    return 0;
+}
